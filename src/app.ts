c2f8b4273524a33a67e9d82cc21b1251b5346e7a
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-02-24 12:55:44 +0600

    middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import cookieParser from 'cookie-parser'
 import express, { Application } from 'express'
 import router from './app/routes'
-const app:Application = express()
+import globalErrorHandler from './app/middlewares/globalErrorhandler'
+import notFound from './app/middlewares/notFound'
+const app: Application = express()
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api',router)
+app.use('/api', router)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-export default app;
+app.use(globalErrorHandler)
+
+//Not Found
+app.use((req, res, next) => {
+  notFound(req, res, next)
+})
+export default app
---
+++ src/app/middlewares/globalErrorhandler.ts
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ErrorRequestHandler } from "express";
+import { TErrorSources } from "../interface/error";
+import { ZodError } from "zod";
+import AppError from "../error/AppError";
+import config from "../config";
+import handleZodError from "../error/handleZodError";
+import handleValidationError from "../error/handleValidationError";
+import handleCastError from "../error/handleCastError";
+import handleDuplicateError from "../error/handleDuplicateError";
+
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): void => {
+  // console.log(err.statusCode);
+  //setting default values
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  }
+
+  //ultimate return
+   res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  });
+};
+
+export default globalErrorHandler;
+
+
